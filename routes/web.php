e54df33ad4265081f10b61d850fbe5174d6563b6
Shadow Grimes <grimeswongatwork@gmail.com>
2017-12-03 16:33:13 +0000

    Setup a empty project

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('main');
+});
+
+Route::post('add_profile_action', function () {
+    $firstname = request('firstName');
+    $lastname = request('lastName');
+    $position = request('position');
+    $email = request('email');
+    $biography = request('biography');
+    $image = request('image');
+    // dd($image);   //debugger
+
+    return view('profile')->with('firstname', $firstname)->with('lastname', $lastname)->with('position', $position)->with('email', $email)->with('biography', $biography)->with('image', $image);
+});
