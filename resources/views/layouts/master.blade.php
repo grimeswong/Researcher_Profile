2a99578d52b245a2e4a406206b6eb52f1ea399a3
Shadow Grimes <grimeswongatwork@gmail.com>
2017-12-08 18:01:06 +0000

    added a loader for transition between loading page and fully loaded page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,22 +12,36 @@
        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+       <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     </head>
 
     <body>
-      <div class="header">
-          <a class="col-sm-4 offset-sm-2" href="{{asset('/')}}">Researchers' Profile</a>
-      </div> <!-- end of header -->
 
-      @yield('content')
+      <div class="preload"> <!-- for preload screen -->
+        <div class="loader">
 
-      <div class="footer">
-          copyright 2017
+        </div>
       </div>
+
+      <!-- <div class"body-wrapper" style="display: none;"> -->
+        <div class="header">
+            <a class="col-sm-4 offset-sm-2" href="{{asset('/')}}">Researchers' Profile</a>
+        </div> <!-- end of header -->
+
+        @yield('content')
+
+        <div class="footer">
+            copyright 2017
+        </div>
+      <!-- </div> -->
     </body>
 </html>
 
 <script type="text/javascript">
+$(document).ready(function(){
+  jQuery(".preload").fadeOut(3000);
+});
+
   var ypos, image;
   function parallax () {
       ypos = window.pageYOffset;
