aa5e1b49b24ed99a14e464bcf95fa7bca70e1827
Shadow Grimes <grimeswongatwork@gmail.com>
2017-12-16 04:05:36 +0000

    Correct the route to index page by the Controller

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,29 @@
 |
 */
 
+/*** Redirect to post index page ***/
+Route::get('/', function () {
+    return redirect('/researcher');
+});
+
+Route::resource('researcher', 'ResearcherController');
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('main');
-});
-
-Route::post('add_profile_action', function () {
-    $firstname = request('firstName');
-    $lastname = request('lastName');
-    $position = request('position');
-    $email = request('email');
-    $biography = request('biography');
-    $image_path = request()->file('image')->store('researcher_images', 'public');
-    // dd($image_path);
-
-    return view('profile')->with('firstname', $firstname)->with('lastname', $lastname)->with('position', $position)->with('email', $email)->with('biography', $biography)->with('image', $image_path);
-});
+// Route::get('/', function () {
+//     return view('main');
+// });
+//
+// Route::post('add_profile_action', function () {
+//     $firstname = request('firstName');
+//     $lastname = request('lastName');
+//     $position = request('position');
+//     $email = request('email');
+//     $biography = request('biography');
+//     $image_path = request()->file('image')->store('researcher_images', 'public');
+//     // dd($image_path);
+//
+//     return view('profile')->with('firstname', $firstname)->with('lastname', $lastname)->with('position', $position)->with('email', $email)->with('biography', $biography)->with('image', $image_path);
+// });
